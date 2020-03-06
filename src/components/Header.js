d01e218e4ea62af83c420e0df0e4bf7deef318e3
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2020-03-06 22:25:20 +0200

    install sass, style the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../stylesheets/header.scss';
 
 const Header = () => (
   <header>
-    <h1>Would You Rather</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">NewQuestion</NavLink>
-    <NavLink to="/top3" activeClassName="is-active">LeaderBoard</NavLink>
-    <NavLink to="/login" activeClassName="is-active">Login</NavLink>
+    <h1 className="title">Would You Rather</h1>
+    <div className="links-wrapper">
+      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+      <NavLink to="/create" activeClassName="is-active">NewQuestion</NavLink>
+      <NavLink to="/top3" activeClassName="is-active">LeaderBoard</NavLink>
+      <NavLink to="/login" activeClassName="is-active">Login</NavLink>
+    </div>
   </header>
 );
 
