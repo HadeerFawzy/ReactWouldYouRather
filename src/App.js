2e64c70f7bb490d44f2ac815ed4e17ddfb5e10ef
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2020-03-13 20:17:39 +0200

    create and add the middleware

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
-import AppRouter from './routers/AppRouter';
-import './stylesheets/App.css';
+// import React from 'react';
+// import { Provider } from 'react-redux';
+// import AppRouter from './routers/AppRouter';
+// import './stylesheets/App.css';
 
-const store = configureStore();
 
-const state = store.getState();
+// const App = () => (
+//   <Provider store={store}>
+//     <AppRouter />
+//   </Provider>
+// );
 
-const App = () => (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
-);
-
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import reducer from './reducers'
+import middleware from './middleware'
+import AppRouter from './routers/AppRouter';
 import './stylesheets/index.css';
-import App from './App';
+import './stylesheets/App.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  document.getElementById('root')
+)
+
 serviceWorker.unregister();
