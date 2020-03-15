b5bdd05bdac990eef4565a1a46be0314f3a45518
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2020-03-15 22:53:05 +0200

    pass authdUser and questions (filtered) to the dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-const Dashboard = () => (
-  <div>
-    Dashboard
-  </div>
-);
+class Dashboard extends Component {
+  
+  render() {
+    const user = this.props.user;
+    const answeredQuestions = this.props.answeredQuestions;
+    const unAnsweredQuestions = this.props.unAnsweredQuestions;
+    console.log("USER", user)
+    console.log("answeredQuestions", answeredQuestions)
+    console.log("unAnsweredQuestions", unAnsweredQuestions)
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+};
 
-export default Dashboard;
+
+function mapStateToProps ({ authedUser, users, questions }) {
+  const user = users[authedUser]  
+  const answeredQuestions = Object.keys(questions).length !== 0
+        ? Object.keys(user.answers).sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+        : []
+
+    const unAnsweredQuestions = Object.keys(questions).length !== 0
+        ? Object.keys(questions).filter(question => !answeredQuestions.includes(question)).sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+        : []
+  return {
+    user,
+    answeredQuestions,
+    unAnsweredQuestions
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
